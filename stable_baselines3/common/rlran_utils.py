f9b8ca19b545f9e8856cfda30b3bb9ee23ce0bc1
lucius yu <eyulush@gmail.com>
2023-03-16 11:29:57 +0100

    update rlran_util.py to support mask q values on multi_actions

--- stable_baselines3/common/rlran_utils.py
+++ stable_baselines3/common/rlran_utils.py
@@ -15,21 +15,64 @@ import stable_baselines3 as sb3
 from stable_baselines3.common.logger import Logger, configure
 from stable_baselines3.common.type_aliases import GymEnv, Schedule, TensorDict, TrainFreq, TrainFrequencyUnit
 
-
 # mask q value only when RLRAN_ENV_ID == 'rlran-ifaas-v0'
 RLRAN_ENV_ID = os.environ.get("RLRAN_ENV_ID", "")
 
-# lambda function to extract backends from observation, it is correlated to your state space design
-get_backends=lambda obs: obs[:,8:13] if obs.ndim==2 else obs[8:13]
-
-# lambda function to calculate mask. if backends of service>1 then it could scaled down. if backends of service < 5 then it could be scaled up. it is correlated to your action mappsing
-get_mask=lambda obs: np.apply_along_axis(lambda backends: np.array([True]+[True if v<5 else False for v in backends]+[True if v>1 else False for v in backends]), axis=obs.ndim-1,arr=obs)
-
+def get_mask(observations: Union[np.ndarray, list]):
+    apps = ['amf', 'cellf', 'ngf', 'uef', 'uplane']
+    action_params = [ { "action": "None", "service_types": "None" },
+            { "action": "up", "service_types": [ "amf" ] },
+            { "action": "up", "service_types": [ "cellf" ] },
+            { "action": "up", "service_types": [ "amf", "cellf" ] },
+            { "action": "up", "service_types": [ "ngf" ] },
+            { "action": "up", "service_types": [ "amf", "ngf" ] },
+            { "action": "up", "service_types": [ "cellf", "ngf" ] },
+            { "action": "up", "service_types": [ "uef" ] },
+            { "action": "up", "service_types": [ "amf", "uef" ] },
+            { "action": "up", "service_types": [ "cellf", "uef" ] },
+            { "action": "up", "service_types": [ "ngf", "uef" ] },
+            { "action": "up", "service_types": [ "uplane" ] },
+            { "action": "up", "service_types": [ "amf", "uplane" ] },
+            { "action": "up", "service_types": [ "cellf", "uplane" ] },
+            { "action": "up", "service_types": [ "ngf", "uplane" ] },
+            { "action": "up", "service_types": [ "uef", "uplane" ] },
+            { "action": "down", "service_types": [ "amf" ] },
+            { "action": "down", "service_types": [ "cellf" ] },
+            { "action": "down", "service_types": [ "amf", "cellf" ] },
+            { "action": "down", "service_types": [ "ngf" ] },
+            { "action": "down", "service_types": [ "amf", "ngf" ] },
+            { "action": "down", "service_types": [ "cellf", "ngf" ] },
+            { "action": "down", "service_types": [ "uef" ] },
+            { "action": "down", "service_types": [ "amf", "uef" ] },
+            { "action": "down", "service_types": [ "cellf", "uef" ] },
+            { "action": "down", "service_types": [ "ngf", "uef" ] },
+            { "action": "down", "service_types": [ "uplane" ] },
+            { "action": "down", "service_types": [ "amf", "uplane" ] },
+            { "action": "down", "service_types": [ "cellf", "uplane" ] },
+            { "action": "down", "service_types": [ "ngf", "uplane" ] },
+            { "action": "down", "service_types": [ "uef", "uplane" ] }
+    ]
+    # get a single mask
+    def get_single_mask(backends):
+        result = []
+        for act in action_params:
+            act_res = True
+            for svc in act['service_types']:
+                if act['action'] == "up":
+                    act_res = act_res and (backends[apps.index(svc)]<5)
+                if act['action'] == "down":
+                    act_res = act_res and (backends[apps.index(svc)]>1)
+            result.append(act_res)
+        return result
+    # main function
+    observations = np.array(observations) if type(observations) == list else observations
+    if observations.ndim == 2:
+        return np.array([get_single_mask(obs[8:13]) for obs in observations])
+    if observations.ndim == 1:
+        return np.array(get_single_mask(observations[8:13]))
 
 # mask q values function
-def mask_q_values(
-    batch_observations: Union[np.ndarray, Dict[Union[str, int], np.ndarray]], batch_q_values: th.Tensor
-) -> th.Tensor:
+def mask_q_values(batch_observations: Union[np.ndarray, Dict[Union[str, int], np.ndarray]], batch_q_values: th.Tensor) -> th.Tensor:
     """
     Fetch mask information from obs, then mask out some q values and return
 
@@ -37,27 +80,29 @@ def mask_q_values(
     :param q_values
     :return: PyTorch tensor of the q_values, in it some q values has been replaced as mininum q value.
     """
-
     # return q values directly if env is not rlran-ifaas-v0
     if RLRAN_ENV_ID != 'rlran-ifaas-v0':
+        print("Warning: mask_q_values only support gym env rlran-ifaas-v0")
+        return batch_q_values
+
+    if type(batch_observations) != np.ndarray:
+        print("Warning: mask_q_values only support observation as type np.ndarray")
         return batch_q_values
 
     # mask q values with min of q values for actions which is not possible to execute
+    batch_masks = get_mask(batch_observations) 
     masked_q_values = None
-    for backends, q_values in zip(get_backends(batch_observations), batch_q_values):
-        mask = th.unsqueeze(th.tensor(get_mask(backends)),0)
+    for mask, q_values in zip(batch_masks, batch_q_values):
+        mask = th.unsqueeze(th.tensor(mask),0)
         min_q_value = q_values.min()
         relative_q_values = q_values - min_q_value 
         relative_q_values = mask * relative_q_values
         masked_q_values = relative_q_values + min_q_value if masked_q_values == None else th.cat((masked_q_values, relative_q_values + min_q_value), dim=0)
-
     return masked_q_values
 
 
 # check if the action is allowed according to backends information
-def is_allowed_action(
-    observation: Union[np.ndarray, Dict[Union[str, int], np.ndarray]], action: Union[np.ndarray,int]
-) -> bool:
+def is_allowed_action(observation: Union[np.ndarray, Dict[Union[str, int], np.ndarray]], action: Union[np.ndarray,int]) -> bool:
     """
     Fetch mask information from obs, then mask out some q values and return
 
@@ -72,13 +117,15 @@ def is_allowed_action(
 
     # return q values directly if env is not rlran-ifaas-v0
     if RLRAN_ENV_ID != 'rlran-ifaas-v0':
-        print(f'RLRAN_ENV_ID={RLRAN_ENV_ID}')
+        print(f"Warning: mask_q_values only support gym env rlran-ifaas-v0, not {RLRAN_ENV_ID}")
         return True
 
-    # calculate True False mask
-    backends = get_backends(observation).squeeze()
-    mask = get_mask(backends)
-    
+    if type(observation) != np.ndarray:
+        print(f"Warning: mask_q_values only support observation as type np.ndarray, not {type(observation)}")
+        return True
+
+    # get mask from observation
+    mask = get_mask(observation)
     # return masked action
     if type(action) == np.ndarray or type(action) == list:
         return all([mask[act] for act in action]) if len(action) > 0 else False
